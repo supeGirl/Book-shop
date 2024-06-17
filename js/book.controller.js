7f18bfb5d78752741e7aef9c7e076c325e89e3ed
supaGirl <kerenvasserman.bm@gmail.com>
2024-06-18 00:02:27 +0300

    Add dynamic rendering table of books and remove the hard coded table from index.html

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,6 +5,30 @@ function onInit(){
 }
 
 function renderBooks(){
-    console.log('hi')
+    const elBooksList = document.querySelector('.books-list')
+    
+    const titleRow = `
+    <thead>
+      <tr class="title-row">
+        <th>Title</th>
+        <th>Price</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+  `
+   const bookRow = gBooks.map((book,idx) => `<tbody>
+        <tr class = "${idx === 1 ? 'second-row' : ''}"> 
+   <td>${book.title}</td>
+   <td>${book.price}</td>
+   <td>
+   <button class="read">Read</button>
+   <button class="update">Update</button>
+   <button class="delete">Delete</button>
+   </td>
+   </tr>
+   </tbody>
+    `)
+   const srtHtml = titleRow + bookRow.join('')
+   elBooksList.innerHTML = srtHtml
  
 }
