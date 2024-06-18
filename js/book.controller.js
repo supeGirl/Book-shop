cac7c303d845e1cd1c8013c34e1aa656f1d09db2
supaGirl <kerenvasserman.bm@gmail.com>
2024-06-18 13:25:40 +0300

    Add removeBook()

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,13 +1,13 @@
 'use strict'
 
-function onInit(){
- renderBooks()   
+function onInit() {
+  renderBooks()
 }
 
-function renderBooks(){
-    const elBooksList = document.querySelector('.books-list')
-    
-    const titleRow = `
+function renderBooks() {
+  const elBooksList = document.querySelector('.books-list')
+
+  const titleRow = `
     <thead>
       <tr class="title-row">
         <th>Title</th>
@@ -16,19 +16,31 @@ function renderBooks(){
       </tr>
     </thead>
   `
-   const bookRow = gBooks.map((book,idx) => `<tbody>
+  const bookRow = gBooks.map(
+    (book, idx) => `<tbody>
         <tr class = "${idx === 1 ? 'second-row' : ''}"> 
    <td>${book.title}</td>
    <td>${book.price}</td>
    <td>
    <button class="read">Read</button>
    <button class="update">Update</button>
-   <button class="delete">Delete</button>
+   <button class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
    </td>
    </tr>
    </tbody>
-    `)
-   const srtHtml = titleRow + bookRow.join('')
-   elBooksList.innerHTML = srtHtml
- 
+    `
+  )
+  const srtHtml = titleRow + bookRow.join('')
+  elBooksList.innerHTML = srtHtml
+}
+
+
+function onRemoveBook(bookId){
+    const isBookRemoved = removeBook(bookId)
+
+    if(isBookRemoved){
+        renderBooks()
+    }else{
+        alert(`Failed to remove book with ID ${bookId}.`)
+    }
 }
--- js/services/book.service.js
+++ js/services/book.service.js
@@ -8,21 +8,31 @@ var gBooks = [
     imgUrl: 'lori-ipsi.jpg',
   },
   {
-    id: 'bg4J78',
+    id: 'bg4J77',
     title: 'World Atlas',
     price: 300,
     imgUrl: 'world-atlas.jpg',
   },
   {
-    id: 'bg4J78',
+    id: 'bg4J75',
     title: 'Zorba the Greek',
     price: 87,
     imgUrl: 'zorba-the-greek.jpg',
   },
 ]
 const showBooks = getBooks()
-console.log('The books are:',showBooks)
+console.log('The books are:', showBooks)
 
 function getBooks() {
   return gBooks
+}
+
+function removeBook(bookId) {
+  const idx = gBooks.findIndex(book => book.id === bookId)
+  if (idx !== -1) {
+    gBooks.splice(idx, 1)
+    return true
+  } else {
+    return false
+  }
 }
